ec59ac466fe6099b7c01109f7b05100a35d75fba
lhz0707 <885297680@qq.com>
2019-10-10 09:50:12 +0800

    zero

--- meiduo_mall/celery_tasks/config.py
+++
@@ -1,3 +0,0 @@
-# 配置任务队列 redis list
-broker_url= 'redis://127.0.0.1:6379/14'
-
--- meiduo_mall/meiduo_mall/apps/contents/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from . import views
-urlpatterns = [
-    url(r'^img/$', views.ImageView.as_view()),
-    # 首页处理
-    url(r'^$', views.IndexView.as_view()),
-]
